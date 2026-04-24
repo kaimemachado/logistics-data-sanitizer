e03364671f5e91ce06acd30f649af06858a71606
Kaime Machado <104177517+kaimemachado@users.noreply.github.com>
2026-04-24 23:54:55 +0000

    feat: build ETL pipeline to process and price inventory data

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
-stock = [{
-            'name' : 'Playstation 5',
-            'sku' : 'PS5elet',
-            'category' : 'Eletronics',
-            'cost_price' : 2000},
-           {
-            'name' : 'Iphone 17',
-            'sku' : 'IP17elet',
-            'category' : 'Eletronics',
-            'cost_price' : 1000},
-            {
-            'name' : 'MacBook',
-            'sku' : 'MCelet',
-            'category' : 'Eletronics',
-            'cost_price' : 4000}]
+import csv
+with open("product_catalog.csv", encoding='utf-8') as file:
+    reader = csv.DictReader(file)
+    gross_inventory = list(reader)
 
-#LOOP PARA PERCORRER O ESTOQUE
-for produto in stock:
-    produto['sku'] = produto['sku'].upper()
-    produto['sale_price'] = produto['cost_price'] * 1.30
-    print(f'O produto {produto["name"]}, de SKU {produto["sku"]} custa R${produto["cost_price"]} mas é vendido por R${produto["sale_price"]:.2f} com 30% de lucro')
+def process_inventory(list_products):
+    for product in list_products:
+        product['sku'] = product['sku'].upper()
+        product['sale_price'] = float(product['cost_price']) * 1.30
+        print(f'O product {product["name"]}, de SKU {product["sku"]} custa R${product["cost_price"]} mas é vendido por R${product["sale_price"]:.2f} com 30% de lucro')
+    return list_products
 
+clean_inventory = process_inventory(gross_inventory)
+
+with open("priced_inventory.csv", mode = "w", encoding="utf-8", newline="") as output_file:
+    columns=['name', 'sku','category','cost_price','sale_price']
+    writer = csv.DictWriter(output_file, fieldnames=columns)
+    
+    writer.writeheader()
+    writer.writerows(clean_inventory)
+
+total_products = len(clean_inventory)
+
+print("--------------------------------------------------")
+print(f"SUCESSO: Processamento concluído!")
+print(f"Total de itens atualizados: {total_products}")
+print(f"Arquivo 'priced_inventory.csv' gerado com sucesso.")
+print("--------------------------------------------------")
